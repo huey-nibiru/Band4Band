078e3e1b81c0390afca32c147dd9327a4275c567
Yousef Macer <yousefmacer@gmail.com>
2023-09-23 11:57:41 -0400

    establish metamask connection

--- band-4-band/src/components/header/Header.tsx
+++ band-4-band/src/components/header/Header.tsx
@@ -1,14 +1,55 @@
-// src/components/Header.tsx
-import React from "react";
+import React, { useCallback, useState } from "react";
 import "./Header.css";
-import ConnectButton from "../metamask/Button";
 
 const Header: React.FC = () => {
+	const [connected, setConnected] = useState(false);
+	const [walletAddress, setWalletAddress] = useState<string | null>(null);
+
+	function formatWalletAddress(walletAddress: string | null): string {
+		if (!walletAddress) return "";
+		if (walletAddress.length <= 9) return walletAddress; // Return the full address if it's very short
+
+		const firstFive = walletAddress.slice(0, 5);
+		const lastFour = walletAddress.slice(-4);
+		return `${firstFive}...${lastFour}`;
+	}
+
+	const connectToMetaMask = useCallback(async () => {
+		try {
+			// Check if MetaMask is installed
+			if (window.ethereum) {
+				await window.ethereum.send("eth_requestAccounts");
+				const accounts = await window.ethereum.request({
+					method: "eth_accounts",
+				});
+				if (accounts && accounts.length > 0) {
+					setWalletAddress(accounts[0]);
+					setConnected(true);
+				}
+			} else {
+				alert("MetaMask is not installed.");
+			}
+		} catch (error) {
+			console.error(error);
+			alert("Error connecting to MetaMask.");
+		}
+	}, []);
+
 	return (
 		<>
 			<header className="navbar">
 				<div className="container">BAND-4-BAND</div>
-				<button className="connect-button">Connect to Metamask</button>
+				{connected ? (
+					<div className="connected">
+						Connected
+						<br />
+						{formatWalletAddress(walletAddress)}
+					</div>
+				) : (
+					<button className="connect-button" onClick={connectToMetaMask}>
+						Connect to MetaMask
+					</button>
+				)}
 			</header>
 		</>
 	);
