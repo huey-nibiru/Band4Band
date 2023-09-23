9bad61e09317123f14160242c12d88cba68987b6
Yousef Macer <yousefmacer@gmail.com>
2023-09-23 10:59:29 -0400

    homepage init

---
+++ band-4-band/src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+// src/components/Header.tsx
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="navbar">
+      <div className="container">
+        BAND-4-BAND
+      </div>
+    </header>
+  );
+};
+
+export default Header;
