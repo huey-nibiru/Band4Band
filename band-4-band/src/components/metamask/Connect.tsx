57ad11e787809d2d47b3ad9e7b6342a4ef9ea7f7
Yousef Macer <yousefmacer@gmail.com>
2023-09-23 23:01:38 -0400

    Homepage and connection Routing

--- band-4-band/src/components/metamask/Connect.tsx
+++ band-4-band/src/components/metamask/Connect.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./Connect.css";
@@ -6,13 +6,13 @@ import "./Connect.css";
 const Connectbutton: React.FC = () => {
 	const [connected, setConnected] = useState(false);
 	const navigate = useNavigate();
-
 	const [walletAddress, setWalletAddress] = useState<string | null>(null);
+
 	console.log("connected:", connected);
 
 	function formatWalletAddress(walletAddress: string | null): string {
 		if (!walletAddress) return "";
-		if (walletAddress.length <= 9) return walletAddress; // Return the full address if it's very short
+		if (walletAddress.length <= 9) return walletAddress;
 		const firstFive = walletAddress.slice(0, 5);
 		const lastFour = walletAddress.slice(-4);
 		return `${firstFive}...${lastFour}`;
@@ -20,8 +20,6 @@ const Connectbutton: React.FC = () => {
 
 	const connectToMetaMask = useCallback(async () => {
 		try {
-			// Check if MetaMask is installed
-
 			if (window.ethereum) {
 				await window.ethereum.send("eth_requestAccounts");
 				const accounts = await window.ethereum.request({
@@ -34,12 +32,44 @@ const Connectbutton: React.FC = () => {
 				}
 			} else {
 				alert("MetaMask is not installed.");
+				navigate("/");
 			}
 		} catch (error) {
 			console.error(error);
 			alert("Error connecting to MetaMask.");
 		}
-	}, []);
+	}, [navigate]);
+
+	const handleAccountsChanged = (accounts: string[]) => {
+		if (accounts.length === 0) {
+			// User disconnected MetaMask
+			setConnected(false);
+			setWalletAddress(null);
+			navigate("/");
+		} else {
+			// User connected or switched accounts
+			setConnected(true);
+			setWalletAddress(accounts[0]);
+		}
+	};
+
+	useEffect(() => {
+		if (window.ethereum) {
+			window.ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+				if (accounts && accounts.length > 0) {
+					setConnected(true);
+					setWalletAddress(accounts[0]);
+					navigate("/connected");
+				} else {
+					navigate("/");
+				}
+
+				window.ethereum.on("accountsChanged", handleAccountsChanged);
+			});
+		} else {
+			navigate("/");
+		}
+	}, [navigate]);
 
 	return (
 		<>
@@ -48,7 +78,6 @@ const Connectbutton: React.FC = () => {
 					Connected
 					<br />
 					{formatWalletAddress(walletAddress)}
-					{/* Render the ProfileSettings component */}
 				</div>
 			) : (
 				<button className="connect-button" onClick={connectToMetaMask}>
