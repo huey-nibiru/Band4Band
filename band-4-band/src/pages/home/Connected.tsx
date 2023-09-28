755a7eb914835e4588a806152a879e21f9c3e738
Yousef Macer <yousefmacer@gmail.com>
2023-09-28 18:52:44 -0400

    Update Connected.tsx

--- band-4-band/src/pages/home/Connected.tsx
+++ band-4-band/src/pages/home/Connected.tsx
@@ -1,6 +1,5 @@
 import Header from "../../components/header/Header";
 import WalletData from "../../components/metamask/Walletdata";
-import InstructionBox from "../../components/Instructions/Instructions";
 
 const ConnectedPage: React.FC = () => {
 	return (
