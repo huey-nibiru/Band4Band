a8ae00411123447cfd98ec1e52b06b2acdaaa02e
Yousef Macer <yousefmacer@gmail.com>
2023-09-29 22:31:35 -0400

    Update Walletdata.tsx

--- band-4-band/src/components/metamask/Walletdata.tsx
+++ band-4-band/src/components/metamask/Walletdata.tsx
@@ -5,7 +5,7 @@ import "./Walletdata.css";
 const Walletdata: React.FC = () => {
 	return (
 		<div className="glassmorphism-box">
-			<h1>WALLET DATA</h1>
+			<h1>Profile</h1>
 			<div className="wallet-details"></div>
 		</div>
 	);
